0b14a63115d8f6241e4059ab9ee8a9dafdd468c7
Amal Jose <amaljose96@gmail.com>
2023-05-14 22:22:48 -0700

    Revert back.

--- src/Portfolio/components/TextInput/index.js
+++ src/Portfolio/components/TextInput/index.js
@@ -5,8 +5,6 @@ function TextInput({
   value,
   setValue,
   placeholder = "Placeholder",
-  type = "text",
-  disabled = false,
   validator = () => true,
   isValid = null,
   isTextArea = false,
@@ -14,22 +12,15 @@ function TextInput({
   hidden=false,
   setIsValid = () => { }
 }) {
-  return <input value={value}  onChange={(e) => {
-    setValue(e.target.value);
-    // setIsValid(validator(e.target.value));
-  }}
   
   
-  />
   let Component = isTextArea ? TextArea : Input;
   return <TextInputContainer>
     <Component
       isValid={isValid}
       value={value}
       placeholder={placeholder}
-      type={type}
       isTextArea={isTextArea}
-      disabled={disabled}
       name={name}
       hidden={hidden}
       onChange={(e) => {
