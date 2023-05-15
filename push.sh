1ee3fef8f23cbcee427641db0223368b4a42c61a
Amal Jose <amaljose96@gmail.com>
2023-05-14 21:08:10 -0700

    Testing auto update

--- push.sh
+++ push.sh
@@ -1,8 +1,9 @@
+message=$(git log -1 --pretty=format:"%an, %s"  $*)
 npm run git-info
 npm run build
 rm -rf ../amaljose96.github.io/*
 cp -R build/* ../amaljose96.github.io/
 cd ../amaljose96.github.io
 git add .
-git commit 
+git commit -m $message
 git push
